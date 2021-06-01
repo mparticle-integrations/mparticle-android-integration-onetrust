c02ed89bd3227ad39e4a6e6e3fe36757c8db7f38
Will Passidomo <wpassidomo@mparticle.com>
2021-06-01 11:43:28 -0400

    null-check user and defer consent application

--- src/main/java/com/mparticle/kits/OneTrustKit.java
+++ src/main/java/com/mparticle/kits/OneTrustKit.java
@@ -10,9 +10,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.mparticle.MParticle;
 import com.mparticle.consent.ConsentState;
 import com.mparticle.consent.GDPRConsent;
+import com.mparticle.identity.IdentityStateListener;
 import com.mparticle.identity.MParticleUser;
 import com.mparticle.internal.Logger;
 import com.onetrust.otpublishers.headless.Public.Keys.OTBroadcastServiceKeys;
@@ -27,20 +31,29 @@ import java.util.List;
 import java.util.Map;
 
 
-public class OneTrustKit extends KitIntegration {
+public class OneTrustKit extends KitIntegration implements IdentityStateListener {
 
     private final static String MP_MOBILE_CONSENT_GROUPS = "mobileConsentGroups";
     private final static String ONETRUST_PREFS = "OT_mP_Mapping";
-    private Context m_context;
 
     private BroadcastReceiver categoryReceiver;
+    final Map<String, String> consentMapping = new HashMap<>();
+    boolean deferConsentApplication = false;
+
+    @Override
+    public String getName() {
+        return "OneTrust";
+    }
+
+    @Override
+    public List<ReportingMessage> setOptOut(boolean optedOut) {
+        return null;
+    }
 
     @Override
     protected List<ReportingMessage> onKitCreate(Map<String, String> settings, final Context context) {
         // Retrieve mParticle --> OneTrust mapping values
         String mobileMappingValues = settings.get(MP_MOBILE_CONSENT_GROUPS);
-
-        final Map<String, String> consentMapping = new HashMap<String, String>();
 
         if (mobileMappingValues != null) {
             try {
@@ -50,7 +63,6 @@ public class OneTrustKit extends KitIntegration {
                 JSONArray consentJSONArray = new JSONArray(mobileMappingValues);
                 for (int i = 0; i < consentJSONArray.length(); i++) {
                     JSONObject consentJSONObject = consentJSONArray.optJSONObject(i);
-
                     consentMapping.put(consentJSONObject.getString("value"), consentJSONObject.getString("map"));
                 }
 
@@ -69,30 +81,51 @@ public class OneTrustKit extends KitIntegration {
                 Log.i("BroadcastService", "MP OT Intent name: " + intent.getAction() +
                         " status = " + intent.getIntExtra(OTBroadcastServiceKeys.EVENT_STATUS, -1));
 
-                OneTrustKit.this.createConsentEvent(intent.getAction(), intent.getIntExtra(OTBroadcastServiceKeys.EVENT_STATUS, -1));
+                MParticleUser user = getCurrentUser();
+                if (user != null) {
+                    OneTrustKit.this.createConsentEvent(user, intent.getAction(), intent.getIntExtra(OTBroadcastServiceKeys.EVENT_STATUS, -1));
+                } else {
+                    deferConsentApplication = true;
+                }
             }
         };
 
+        MParticleUser user = getCurrentUser();
+        if (user != null) {
+            applyCurrentConsentState(user);
+        } else {
+            deferConsentApplication = true;
+        }
+
+        // Save mapping to disk. Will be retrieved by OneTrust Mobile SDK
+        saveToDisk(mobileMappingValues);
+        return null;
+    }
+
+    @Override
+    public void onUserIdentified(@NonNull MParticleUser user, @Nullable MParticleUser previousUser) {
+        if (deferConsentApplication) {
+            deferConsentApplication = false;
+            applyCurrentConsentState(user);
+        }
+    }
+
+    private void applyCurrentConsentState(final MParticleUser user) {
         for(final String consentElement: consentMapping.keySet()) {
             // Register receiver from above based on cookie value dispatched by OneTrust SDK
-            context.registerReceiver(categoryReceiver, new IntentFilter(consentElement));
+            getContext().registerReceiver(categoryReceiver, new IntentFilter(consentElement));
 
             // Fetch Consent Status from OneTrust based on cookie value
-            new OTPublishersHeadlessSDK(context).getConsentStatusForGroupId(consentElement);
+            new OTPublishersHeadlessSDK(getContext()).getConsentStatusForGroupId(consentElement);
 
             // Dispatch creation of initial consent state till after init is done
             new Handler().post(new Runnable() {
                 @Override
                 public void run() {
-                    OneTrustKit.this.createConsentEvent(consentMapping.get(consentElement), 0);
+                    OneTrustKit.this.createConsentEvent(user, consentMapping.get(consentElement), 0);
                 }
             });
         }
-
-
-        // Save mapping to disk. Will be retrieved by OneTrust Mobile SDK
-        saveToDisk(mobileMappingValues);
-        return null;
     }
 
     // Create an mParticle consent state based on One Trust mapping
@@ -100,33 +133,19 @@ public class OneTrustKit extends KitIntegration {
     //  1 = Consent Given
     //  0 = Consent Not Given
     // -1 = Consent has not been collected/ sdk is not yet initialized
-    private void createConsentEvent(String purpose, Integer status) {
-        MParticleUser user = MParticle.getInstance().Identity().getCurrentUser();
+    private void createConsentEvent(MParticleUser user, String purpose, Integer status) {
 
         GDPRConsent gdprConsent = GDPRConsent
                 .builder(status.intValue() == 1)
                 .timestamp(System.currentTimeMillis())
                 .build();
 
-        ConsentState state = ConsentState
+        final ConsentState state = ConsentState
                 .builder()
                 .addGDPRConsentState(purpose, gdprConsent)
                 .build();
 
         user.setConsentState(state);
-    }
-
-
-    @Override
-    public String getName() {
-        return "OneTrust";
-    }
-
-
-
-    @Override
-    public List<ReportingMessage> setOptOut(boolean optedOut) {
-        return null;
     }
     
      public void saveToDisk(String mappingData){
